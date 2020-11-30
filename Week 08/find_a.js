c6b7719171ed07ecb45a4aac8aeefcd8653803af
IVANLEE99 <ivanlee99@163.com>
2020-11-30 21:02:57 +0800

    4. 状态机 | 不使用状态机处理字符串（二）

--- Week 08/find_a.js
+++ Week 08/find_a.js
@@ -10,5 +10,17 @@ function find_a(string) {
     }
     return false;
 }
-
-console.log(find_a('sdfsdffsasdafsdf'));
+// 不准使用正则表达式，纯粹用 JavaScript 的逻辑实现：在一个字符串中，找到字符“ab”
+function find_ab(string) {
+    if (typeof string != 'string') {
+        return false;
+    }
+    for (let i = 0; i < string.length; i++) {
+        const s = string[i];
+        if (s == 'a' && i + 1 < string.length && string[i + 1] == 'b') {
+            return [i, i + 1];
+        }
+    }
+    return false;
+}
+console.log(find_ab('sdfsdffabsasdafsdf'));
