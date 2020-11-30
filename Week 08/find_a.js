53fcda1c400e60a072e23d62451e020869e7e2e2
IVANLEE99 <ivanlee99@163.com>
2020-11-30 21:15:23 +0800

    5. 状态机 | 不使用状态机处理字符串（三）

--- Week 08/find_a.js
+++ Week 08/find_a.js
@@ -23,4 +23,26 @@ function find_ab(string) {
     }
     return false;
 }
-console.log(find_ab('sdfsdffabsasdafsdf'));
+
+// 不准使用正则表达式，纯粹用 JavaScript 的逻辑实现：在一个字符串中，找到字符“abcdef”
+function find_abcdef(string) {
+    if (typeof string != 'string') {
+        return false;
+    }
+    if (string.length < 'abcdef'.length) {
+        return false;
+    }
+    for (let i = 0; i < string.length; i++) {
+        const s = string[i];
+        if (s == 'a'
+            && (i + 1 < string.length && string[i + 1] == 'b')
+            && (i + 2 < string.length && string[i + 2] == 'c')
+            && (i + 3 < string.length && string[i + 3] == 'd')
+            && (i + 4 < string.length && string[i + 4] == 'e')
+            && (i + 5 < string.length && string[i + 5] == 'f')) {
+            return true;
+        }
+    }
+    return false;
+}
+console.log(find_abcdef('sdfsabcdefdffabsasdafsdf'));
