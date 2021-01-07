303a01aa184896c8a8e30894e19a981192413968
IVANLEE99 <ivanlee99@163.com>
2021-01-07 21:33:43 +0800

    jsx

--- Week 14/jsx/main.js
+++ Week 14/jsx/main.js
@@ -1,5 +1,10 @@
 function createElement(type, attributes, ...children) {
-    let element = document.createElement(type);
+    let element
+    if (typeof type === 'string') {
+        element =new ElementWrapper(type);
+    } else {
+        element = new type;
+    }
     for (const name in attributes) {
         if (attributes.hasOwnProperty(name)) {
             const e = attributes[name];
@@ -8,15 +13,65 @@ function createElement(type, attributes, ...children) {
     }
 
     for (const child of children) {
+        if (typeof child === 'string') {
+            child = new TextWrapper(child);
+        }
         element.appendChild(child);
     }
     return element;
 }
+class ElementWrapper{
+    constructor(type) {
+        this.root = document.createElement(type);
+    }
+    setAttribute(name, value) {
+        this.root.setAttribute(name, value);
+    }
+    appendChild(child) {
+        // this.root.appendChild(child);
+        child.mountTo(this.root);
+    }
+    mountTo(parent) {
+        parent.appendChild(this.root);
+    }
+}
+class TextWrapper{
+    constructor(content) {
+        this.root = document.createTextNode(content);
+    }
+    setAttribute(name, value) {
+        this.root.setAttribute(name, value);
+    }
+    appendChild(child) {
+        // this.root.appendChild(child);
+        child.mountTo(this.root);
 
-let a = <div id="a">
-    <span></span>
-    <span></span>
-    <span></span>
-</div>
+    }
+    mountTo(parent) {
+        parent.appendChild(this.root);
+    }
+}
+class Div {
+    constructor() {
+        this.root = document.createElement("div");
+    }
+    setAttribute(name, value) {
+        this.root.setAttribute(name, value);
+    }
+    appendChild(child) {
+        // this.root.appendChild(child);
+        child.mountTo(this.root)
+    }
+    mountTo(parent) {
+        parent.appendChild(this.root);
+    }
+}
+let a = <Div id="a">
+    <span>a</span>
+    <span>b</span>
+    <span>c</span>
+    hello world
+</Div>
 
-document.body.appendChild(a);
+// document.body.appendChild(a);
+a.mountTo(document.body);
